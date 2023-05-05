544f64de4b83d5b48a9c8eff229d8ebd67019df6
ariiaanee <132436860+ariiaanee@users.noreply.github.com>
2023-05-05 03:14:01 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#ARIANE KAYE
+#ict2A2
+#II INPUT/PROCESS/OUTPUT – Basic grade calculation (20 points)
+#worksheet week no. 1 & 2
+
+mark1 = 85
+mark2 = 75
+
+print("First mark:", mark1)
+print("Second mark:", mark2)
+
+mark3 = float(input("Enter third mark: "))
+print("Third mark:", mark3)
+
+mark4 = float(input("Enter fourth mark: "))
+print("Fourth mark:", mark4)
+
+markTotal = mark1 + mark2 + mark3 + mark4
+print("Total marks:", markTotal)
+
+average = markTotal / 4
+print("Average:", average)
