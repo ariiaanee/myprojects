aa018a1c3843831eecfe8c6f9d8c0a03fb5129f0
ariiaanee <132436860+ariiaanee@users.noreply.github.com>
2023-05-05 04:25:32 -0700

    Add files via upload

---
+++ 3-4.py
@@ -0,0 +1,17 @@
+# Ariane Kaye
+# ICT2A2
+# WORKSHEET WEEK NO 3 - 4
+# III SPECIALIZATION Apply the String Manipulation using Arithmetic Operators 20 points
+# Concatenate the three string “I”, “LOVE” and “YOU” into one value and print
+# the value four times
+
+string1 = "I"
+string2 = "LOVE"
+string3 = "YOU"
+
+# Concatenate the three strings
+result = string1 + " " + string2 + " " + string3
+
+# Print the concatenated string four times
+for i in range(4):
+    print(result)
